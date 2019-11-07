90653c417e22abdc20b2d57c4af132e4e953cd6d
Sue <susanhartesquire@yahoo.com>
2019-11-07 11:08:35 -0500

    undoing some changes from today

--- api/app.js
+++ api/app.js
@@ -370,3 +370,4 @@ app.set("port", process.env.PORT || 5000);
 const server = app.listen(app.get("port"), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
+
---
+++ client/src/components/Courses.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+class Courses extends Component {
+    constructor() {
+      super();
+      this.state = {
+        loading: [],
+        result: []
+      };
+    }
+    render() {
+    return <div> test </div>
+}}
