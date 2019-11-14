70c23567ddc45dc7c689972cad0889edfdd12045
Sue <susanhartesquire@yahoo.com>
2019-11-14 10:37:01 -0500

    commit course detail html

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -36,6 +36,12 @@ class Courses extends Component {
     return (    
     <div>
       {courseList}
+      <div class="grid-33"><a class="course--module course--add--module" href="create-course.html">
+            <h3 class="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 13 13" class="add">
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+              </svg>New Course</h3>
+          </a></div>
     </div>
     )
     }
