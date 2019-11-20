f8850e60fca3a4e6058f4b07d72e6c92cd290895
Sue <susanhartesquire@yahoo.com>
2019-11-20 16:01:49 -0500

    commit Data.js changes

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,7 +1,7 @@
 import config from './Config';
 
 export default class Data {
-  api(path, method = 'GET', body = null) {
+  api(path, method = 'GET', body = null, requiresAuth = false, credentials = null) {
     const url = config.apiBaseUrl + path;
   
     const options = {
@@ -14,12 +14,16 @@ export default class Data {
     if (body !== null) {
       options.body = JSON.stringify(body);
     }
-
+    // Check if auth is required
+  if (requiresAuth) {    
+    const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
+    options.headers['Authorization'] = `Basic ${encodedCredentials}`;
+  }
     return fetch(url, options);
   }
 
-  async getUser() {
-    const response = await this.api(`/users`, 'GET', null);
+  async getUser(username, password) { //add new parameters
+    const response = await this.api(`/users`, 'GET', null, true, { username, password });
     if (response.status === 200) {
       return response.json().then(data => data);
     }
